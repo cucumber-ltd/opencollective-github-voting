280267bfd90c96f8646412a79c077378bf971c80
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-11-13 16:00:58 +0000

    Use projections

--- features/support/assemblies/memory.js
+++ features/support/assemblies/memory.js
@@ -2,10 +2,6 @@ const Assembly = require('../../../lib/Assembly')
 const MemoryEventStore = require('../../../lib/infrastructure/eventstore/MemoryEventStore')
 
 module.exports = class MemoryAssembly extends Assembly {
-  async start() {
-    await super.start()
-  }
-
   makeEventStore() {
     return new MemoryEventStore()
   }
