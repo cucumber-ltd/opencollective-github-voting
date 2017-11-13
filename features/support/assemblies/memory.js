ec977e1f4f7c62dbc146dbe5d82b1675539e507a
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-11-13 16:03:32 +0000

    Rename infrastructure to cqrs-lite

--- features/support/assemblies/memory.js
+++ features/support/assemblies/memory.js
@@ -1,5 +1,5 @@
 const Assembly = require('../../../lib/Assembly')
-const MemoryEventStore = require('../../../lib/infrastructure/eventstore/MemoryEventStore')
+const MemoryEventStore = require('../../../lib/cqrs-lite/eventstore/MemoryEventStore')
 
 module.exports = class MemoryAssembly extends Assembly {
   makeEventStore() {
