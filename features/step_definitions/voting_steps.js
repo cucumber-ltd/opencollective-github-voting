ffb9e331447b26bb4abc0b37ec1aec55201effc0
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-11-13 16:14:08 +0000

    Simplify

--- features/step_definitions/voting_steps.js
+++ features/step_definitions/voting_steps.js
@@ -3,31 +3,31 @@ const { Given, When, Then } = require('cucumber')
 const Account = require('../../lib/domain/entities/Account')
 
 Given('{username} exists', async function createAccount(username) {
-  await this.votingPort().createUser(username)
+  await this.votingPort().createAccount(username)
 })
 
 Given('{issueIdentifier} exists', async function createAccount(issueIdentifier) {
-  await this.votingPort().createIssue(issueIdentifier)
+  await this.votingPort().createAccount(issueIdentifier)
 })
 
 Given('{username} has {int} votes', async function creditAccount(username, amount) {
-  await this.votingPort().creditUser(username, amount)
+  await this.votingPort().creditAccount(username, amount)
 })
 
 Given('{issueIdentifier} has {int} votes', async function creditAccount(issueIdentifier, amount) {
-  await this.votingPort().creditIssue(issueIdentifier, amount)
+  await this.votingPort().creditAccount(issueIdentifier, amount)
 })
 
 When('{username} votes {int} on {issueIdentifier}', async function(username, amount, issueIdentifier) {
-  await this.votingPort().vote(username, issueIdentifier, amount)
+  await this.votingPort().transfer(username, issueIdentifier, amount)
 })
 
 Then('{issueIdentifier} should have {int} votes', async function(issueIdentifier, expectedBalance) {
-  const balance = await this.votingPort().getIssueVotes(issueIdentifier)
+  const balance = await this.votingPort().getBalance(issueIdentifier)
   assert.equal(balance, expectedBalance)
 })
 
 Then('{username} should have {int} votes left', async function(username, expectedBalance) {
-  const balance = await this.votingPort().getUserVotes(username)
+  const balance = await this.votingPort().getBalance(username)
   assert.equal(balance, expectedBalance)
 })
