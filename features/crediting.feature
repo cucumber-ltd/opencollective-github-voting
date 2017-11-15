6adc7a41664804aff4771d32379986fab1dca84a
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-11-15 14:02:59 +0100

    Introduce currencies

---
+++ features/crediting.feature
@@ -0,0 +1,14 @@
+Feature: Crediting
+  There are two ways to get voting power:
+  * "Buy" votes by donating to the OpenCollective
+  * "Earn" votes by being active on GitHub
+
+  Rules
+  =====
+  * One vote for each day since first commit to repo
+
+  Scenario: @aslakhellesoy is credited for days
+    Given @aslakhellesoy exists
+    And @aslakhellesoy has been a committer for 300 days
+    When @aslakhellesoy is credited for committer days
+    Then @aslakhellesoy should have 300 votes left
--- features/step_definitions/voting_steps.js
+++ features/step_definitions/voting_steps.js
@@ -2,32 +2,42 @@ const assert = require('assert')
 const { Given, When, Then } = require('cucumber')
 const Account = require('../../lib/domain/entities/Account')
 
-Given('{username} exists', async function createAccount(username) {
-  await this.contextVotingPort().createAccount(username)
+Given('{username} exists', async function(username) {
+  await this.contextVotingPort().createAccount({ owner: username, currency: 'votes' })
 })
 
-Given('{issueIdentifier} exists', async function createAccount(issueIdentifier) {
-  await this.contextVotingPort().createAccount(issueIdentifier)
+Given('{issueIdentifier} exists', async function(issueIdentifier) {
+  const accountNumber = { owner: issueIdentifier, currency: 'votes' }
+  await this.contextVotingPort().createAccount(accountNumber)
 })
 
-Given('{username} has {int} votes', async function creditAccount(username, amount) {
-  await this.contextVotingPort().creditAccount(username, amount)
+Given('{username} has {int} votes', async function(username, amount) {
+  const accountNumber = { owner: username, currency: 'votes' }
+  await this.contextVotingPort().creditAccount(accountNumber, amount)
 })
 
-Given('{issueIdentifier} has {int} votes', async function creditAccount(issueIdentifier, amount) {
-  await this.contextVotingPort().creditAccount(issueIdentifier, amount)
+Given('{issueIdentifier} has {int} votes', async function(issueIdentifier, amount) {
+  const accountNumber = { owner: issueIdentifier, currency: 'votes' }
+  await this.contextVotingPort().creditAccount(accountNumber, amount)
 })
 
 When('{username} votes {int} on {issueIdentifier}', async function(username, amount, issueIdentifier) {
-  await this.actionVotingPort().transfer(username, issueIdentifier, amount)
+  const fromAccounNumber = { owner: username, currency: 'votes' }
+  const toAccountNumber = {
+    owner: issueIdentifier,
+    currency: 'votes'
+  }
+  await this.actionVotingPort().transfer(fromAccounNumber, toAccountNumber, amount)
 })
 
 Then('{issueIdentifier} should have {int} votes', async function(issueIdentifier, expectedBalance) {
-  const account = await this.outcomeVotingPort().getAccount(issueIdentifier)
+  const accountNumber = { owner: issueIdentifier, currency: 'votes' }
+  const account = await this.outcomeVotingPort().getAccount(accountNumber)
   assert.equal(account.balance, expectedBalance)
 })
 
 Then('{username} should have {int} votes left', async function(username, expectedBalance) {
-  const account = await this.outcomeVotingPort().getAccount(username)
+  const accountNumber = { owner: username, currency: 'votes' }
+  const account = await this.outcomeVotingPort().getAccount(accountNumber)
   assert.equal(account.balance, expectedBalance)
 })
