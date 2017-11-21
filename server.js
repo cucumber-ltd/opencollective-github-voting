71a83525ce482a3a456cfff746995fe3fc93285c
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-11-21 14:30:04 +0000

    WebApp is workin

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 const AppAssembly = require('./lib/AppAssembly')
 const assembly = new AppAssembly()
 const port = parseInt(process.env.PORT || 8080)
-assembly.webServer.listen(port)
-  .then(port => console.log(`http://localhost:${port}`))
-  .catch(err => console.err(err))
+
+async function start() {
+  const listenPort = await assembly.webServer.listen(port)
+  console.log(`http://localhost:${listenPort}`)
+
+  // Set up some accounts (will do as batch jobs later)
+  const A = assembly.accountCommands
+  const X = assembly.transferCommands
+
+  const i250Votes = {owner: 'cucumber/cucumber#250', currency: 'votes'}
+  await A.createAccount(i250Votes)
+
+  const aslakVotes = {owner: '@aslakhellesoy', currency: 'votes'}
+  await A.createAccount(aslakVotes)
+
+  const aslakDollars = {owner: '@aslakhellesoy', currency: 'dollars'}
+  await A.createAccount(aslakDollars)
+  await A.creditAccount(aslakDollars, 500)
+  await X.transfer(aslakDollars, aslakVotes, 400)
+}
+
+start().catch(err => console.err(err))
+
