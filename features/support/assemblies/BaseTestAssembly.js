08807724528ed124a134c28a740bfb2fee9d289e
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-12-11 22:42:01 +0000

    Import commit-days

--- features/support/assemblies/BaseTestAssembly.js
+++ features/support/assemblies/BaseTestAssembly.js
@@ -1,6 +1,6 @@
 const uuid = require('uuid/v4')
 const ServerAssembly = require('../../../lib/ServerAssembly')
-const SimulatingCommitsProvider = require('../../../lib/commits/SimulatingCommitsProvider')
+const SimulatingCommitsProvider = require('../../../lib/commits/SimulatorCommitsProvider')
 
 module.exports = class BaseTestAssembly {
 
---
+++ scripts/download-commits.js
@@ -0,0 +1,14 @@
+const { Transform } = require('stream')
+const GithubCommitsStream = require('../lib/commits/GitHubCommitsStream')
+
+const commitsStream = new GithubCommitsStream({ owner: 'cucumber', repo: 'cucumber', author: 'aslakhellesoy' })
+
+commitsStream.on('error', err => console.error('ERROR', err))
+
+commitsStream
+  .pipe(new Transform({
+    objectMode: true, transform(results, _, cb) {
+      cb(null, JSON.stringify(results) + "\n")
+    }
+  }))
+  .pipe(process.stdout)
